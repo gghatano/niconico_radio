846704e15788f0af2fae7aca7ecd832cce75b12c
gghatano <adad0405@gmail.com>
2014-11-18 10:16:19 +0900

    update README

--- auto_tweet.bash
+++ auto_tweet.bash
@@ -2,19 +2,19 @@
 
 dir=$(dirname $0)
 num=$(ls $dir/contents | wc -l)
-echo $num >> $dir/log.txt
+echo $num >> $dir/tweet.log.txt
 
 while true
 do
   content_num=$(echo $((RANDOM % $num)))
   content=$(cat $dir/contents/$content_num.txt | tr '\n' '%')
-  echo $content
   
   cat $dir/radio_tweet.R | 
   sed "s;test;$content;" | 
   tr '%' '\n' > $dir/radio_tweet.R.test
   
-  R -f $dir/radio_tweet.R.test
+  R -q -f $dir/radio_tweet.R.test > $dir/tmp/log.txt
+
   sleep 600
 done
 
